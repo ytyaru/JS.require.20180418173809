b487e302e64a4db0515572e3def0ecce806b0917
ytyaru <ytyaru@outlook.jp>
2018-04-18 21:06:30 +0900

    遅延読込は非常にむずかしい

--- docs/src/1/js/ext/marked/renderer/Code.js
+++ docs/src/1/js/ext/marked/renderer/Code.js
@@ -7,9 +7,15 @@ define(function(require, exports, module) {
 	    lang_name = Code._Split(language);
 	    // 言語別ハイライト用JS動的読込
 	    // https://stackoverflow.com/questions/17446844/dynamic-require-in-requirejs-getting-module-name-has-not-been-loaded-yet-for-c?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-	    var hllangjs = require('js/lib/highlight/languages/'+Code._ReplaceLanguage(lang_name[0])+'.min');
-	    console.log('読込完了: highlight/languages/'+Code._ReplaceLanguage(lang_name[0])+'.min');
-	    //require(['js/lib/highlight/languages/'+Code._ReplaceLanguage(lang_name[0])+'.min'], function(hllangjs) {console.log('読込完了: highlight/languages/'+Code._ReplaceLanguage(lang_name[0])+'.min');});
+
+	    // 同期できない
+	    // marked.min.js:1 Uncaught Error: Module name "js/lib/highlight/languages/shell.min" has not been loaded yet for context: _
+	    //var hllangjs = require('js/lib/highlight/languages/'+Code._ReplaceLanguage(lang_name[0])+'.min');
+	    //console.log('読込完了: highlight/languages/'+Code._ReplaceLanguage(lang_name[0])+'.min');
+	    require(['js/lib/highlight/languages/'+Code._ReplaceLanguage(lang_name[0])+'.min'], function(hllangjs) {
+		console.log('読込完了: highlight/languages/'+Code._ReplaceLanguage(lang_name[0])+'.min');
+		// 再びmarked()を実行する？　予めmdから必要な言語を取得して動的読込してからパースしたほうがよさそうだが。
+	    });
 	    return Code._MakePreCodeTag(lang_name[0], lang_name[1], code);
 	};
     };
