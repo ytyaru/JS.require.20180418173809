c1a0bc283873fcc0a2d2fae23448416fc1cbe49c
ytyaru <ytyaru@outlook.jp>
2018-04-18 18:52:51 +0900

    ハイライト用言語JSが存在するときだけロード

--- docs/src/1/js/ext/marked/renderer/Code.js
+++ docs/src/1/js/ext/marked/renderer/Code.js
@@ -1,9 +1,13 @@
 define(function(require, exports, module) {
+    var $ = require('jquery');
+    var hljs = require('highlight');
     Code = {};
     Code.Setup = function(renderer) {
-	var hljs = require('highlight');
+	//var hljs = require('highlight');
 	renderer.code = function(code, language) {
 	    lang_name = Code._Split(language);
+	    return Code._MakeTag(lang_name[0], lang_name[1]);
+	    /*
 	    // 言語別ハイライト用JS読込
 	    // https://stackoverflow.com/questions/17446844/dynamic-require-in-requirejs-getting-module-name-has-not-been-loaded-yet-for-c?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
 	    require(['js/lib/highlight/languages/'+lang_name[0]+'.min'], function(hllangjs) {
@@ -11,6 +15,7 @@ define(function(require, exports, module) {
 	    });
 	    //Code._DynamicLoadHllangjs(lang_name[0]);
 	    //return '<pre>' + Code._FileNameTag(lang_name[1])+ '<code class="hljs">' + hljs.highlightAuto(code).value + '</code></pre>';
+	    */
 	};
     };
     // ```lang:filename
@@ -19,8 +24,39 @@ define(function(require, exports, module) {
 	var info = language.split(delimiter);
 	var lang = info.shift();
 	var fileName = info.join(delimiter);
-	return [lang, fileName];
+	return [Code._ReplaceLanguage(lang), fileName];
     };
+    // ```sh とするが shell.min.js という名前である
+    // ```html とするが xml.min.js しかない
+    // * markdownの時点で存在するlanguagesファイルを意識すべき？
+    // * markdownでは実在する拡張子にして存在するlanguagesファイル名に変換すべき？
+    Code._ReplaceLanguage = function(lang) {
+	if ('c' == lang || 'h' == lang || 'cc' == lang || 'hh' == lang || 'hpp' == lang || 'c++' == lang || 'h++' == lang) { return 'cpp'; }
+	if ('html' == lang || 'svg' == lang) { return 'xml'; }
+	else if ('js' == lang) { return 'javascript'; }
+	else if ('md' == lang) { return 'markdown'; }
+	else if ('sh' == lang) { return 'shell'; }
+	else { return lang; }
+    }
+    Code._MakeTag = function(lang, filename) {
+	// languages/*.min.js ファイルが存在したらロードする
+	$.ajax({
+	    url: 'https://ytyaru.github.io/JS.require.20180418173809/src/1/js/lib/highlight/languages/'+lang+'.min.js',
+	})
+	.done(function (response, textStatus, jqXHR) {
+	    require(['js/lib/highlight/languages/'+lang+'.min'], function(hllangjs) {
+		return Code._MakePreCodeTag(lang, filename);
+	    });
+	})
+	.fail(function (jqXHR, textStatus, errorThrown) {
+	    return Code._MakePreCodeTag(lang, filename);
+	})
+	;
+    }
+    // pre.code タグ作成
+    Code._MakePreCodeTag = function(lang, filename) {
+	return '<pre>' + Code._FileNameTag(filename)+ '<code class="hljs">' + hljs.highlightAuto(code).value + '</code></pre>';
+    }
     // ファイル名表示用タグ
     Code._FileNameTag = function(fileName) {
 	if (fileName) { return '<code class="filename">'+fileName+'</code>'; }
