16e5ccc2c82bfaa7c493a1164090b467d46e627c
ytyaru <ytyaru@outlook.jp>
2018-04-18 17:56:50 +0900

    ハイライトをロード

--- docs/src/1/js/ext/marked/renderer/Code.js
+++ docs/src/1/js/ext/marked/renderer/Code.js
@@ -1,8 +1,11 @@
 define(function(require, exports, module) {
     Code = {};
     Code.Setup = function(renderer) {
+	var hljs = require('highlight');
 	renderer.code = function(code, language) {
 	    lang_name = Code._Split(language);
+	    // 言語別ハイライト用JS読込
+	    // https://stackoverflow.com/questions/17446844/dynamic-require-in-requirejs-getting-module-name-has-not-been-loaded-yet-for-c?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
 	    require(['js/lib/highlight/languages/'+lang_name[0]+'.min'], function(hllangjs) {
 		return '<pre>' + Code._FileNameTag(lang_name[1])+ '<code class="hljs">' + hljs.highlightAuto(code).value + '</code></pre>';
 	    });
@@ -18,17 +21,19 @@ define(function(require, exports, module) {
 	var fileName = info.join(delimiter);
 	return [lang, fileName];
     };
+    // ファイル名表示用タグ
+    Code._FileNameTag = function(fileName) {
+	if (fileName) { return '<code class="filename">'+fileName+'</code>'; }
+	else { return ''; }
+    };
+    /*
     // 言語別ハイライト用JS読込
     Code._DynamicLoadHllangjs = function(lang) {
 	// https://stackoverflow.com/questions/17446844/dynamic-require-in-requirejs-getting-module-name-has-not-been-loaded-yet-for-c?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
 	var hllangjs = require('js/lib/highlight/languages/'+lang+'.min');
 	//var hllangjs = require('js/lib/highlight/languages/'+language+'.js');
     };
-    // ファイル名表示用タグ
-    Code._FileNameTag = function(fileName) {
-	if (fileName) { return '<code class="filename">'+fileName+'</code>'; }
-	else { return ''; }
-    };
+    */
     return Code;
     /*
     return {
